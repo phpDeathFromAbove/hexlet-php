c727f76aadb819be3cf2d69d230df82304490d50
ALex <veter_liliputov@citydom.ru>
2024-10-27 23:27:04 +0700

    Add wrong test 2(removed leter)

---
+++ src/StringUtils.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function capitalize($text)
+{
+    if ($text === '') {
+        return '';
+    }
+    $firstSymbol = mb_strtoupper($text[0]);
+    $restSubstring = mb_substr($text, 1);
+    return "{$firstSymbol}{$restSubstring}";
+}
+
